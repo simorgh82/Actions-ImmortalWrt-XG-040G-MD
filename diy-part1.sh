b7090989657d13b1dd0bfd99e56de62f5578f026
simorgh82 <cumtbio@gmail.com>
2026-05-31 22:05:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+PATCHES_DIR="$GITHUB_WORKSPACE/patches"
+USER_BRANCH="$1"
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+echo "用户选择分支: $USER_BRANCH"
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+if [ "$USER_BRANCH" = "master-XG-040G-MD" ]; then
+    PATCH_FILE="$PATCHES_DIR/airoha-en7581-support-master.patch"
+elif [ "$USER_BRANCH" = "openwrt-25.12-XG-040G-MD" ]; then
+    PATCH_FILE="$PATCHES_DIR/airoha-en7581-support-25.12.patch"
+else
+    echo "未知分支，跳过补丁"
+    exit 0
+fi
+
+if [ -f "$PATCH_FILE" ]; then
+    echo "正在应用补丁: $PATCH_FILE"
+    patch -p1 < "$PATCH_FILE"
+    if [ $? -eq 0 ]; then
+        echo "补丁应用成功"
+    else
+        echo "补丁应用失败！"
+        exit 1
+    fi
+else
+    echo "错误: 找不到补丁文件 $PATCH_FILE"
+    exit 1
+fi
